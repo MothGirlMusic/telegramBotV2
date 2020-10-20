f67ae579664b9af64b45cb305a2069f2f620403b
Katja DeCuir <Katja.DeCuir@gmail.com>
2020-10-20 18:26:08 -0400

    edit dicerroller for test

--- plugins/diceroller.php
+++ plugins/diceroller.php
@@ -17,8 +17,12 @@ class diceroller
 		case "messageRecived":
 			// Do something with the Data
 			$received_message = $data->{'message'}->{'text'};
-			if ($received_message == 'ping') {
-				$answer = "replay|PONG!";
+			if ($received_message == 'roll') {
+                                $factory = new Factory();
+                                $pool = $factory->newInstance('2D6+3');
+                                $note = $pool->notation();    // returns 2D6+3
+                                $dieroll = $pool->roll()->value(); // returns 6
+				$answer = "replay|".$note." = ".$dieroll;
 				return $answer;
 			}
 			break;
